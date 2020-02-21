73a0335ca07fb4cbf58f0a70d75b58b741773f34
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2020-02-21 14:40:00 -0800

    v25 (Refinement): Implement BlockingQueueSplit spec in Java and C.

--- impl/producer_consumer.c
+++ impl/producer_consumer.c
@@ -11,7 +11,7 @@ uint32_t *buffer;
 uint32_t fillIndex, useIndex, count = 0;
 
 // See https://stackoverflow.com/a/2087046/6291195 to relate this to the Java impl.
-pthread_cond_t modify;
+pthread_cond_t empty, full; 
 pthread_mutex_t mutex;
 
 void append(uint32_t value) {
@@ -31,12 +31,12 @@ void *producer (void * arg) {
 	while(1) {
 		pthread_mutex_lock(&mutex);   // acquire the lock	
 		while (count == buff_size)    // check if the buffer is full
-		    pthread_cond_wait(&modify, &mutex);
+		    pthread_cond_wait(&empty, &mutex);
 		
 		append(rand() % (10));        // produce!
 
-		pthread_cond_signal(&modify); // broadcast that the buffer is full
-        pthread_mutex_unlock(&mutex); // release the lock
+		pthread_cond_signal(&full); // broadcast that the buffer is full
+        	pthread_mutex_unlock(&mutex); // release the lock
 	}
 }
 
@@ -46,10 +46,10 @@ void *consumer (void * arg) {
 		pthread_mutex_lock(&mutex);   // acquire the lock
 
 		while (count == 0)            // check if the buffer is empty
-			pthread_cond_wait(&modify, &mutex); // wait for the buffer to be filled
+			pthread_cond_wait(&full, &mutex); // wait for the buffer to be filled
 
 		head();                       // consume (we don't care about the value)!
-		pthread_cond_signal(&modify); // signal that the buffer is empty
+		pthread_cond_signal(&empty); // signal that the buffer is empty
 		pthread_mutex_unlock(&mutex); // release the lock
 		
 		if (report++ % 10000 == 0) {
